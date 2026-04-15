ae11aeb0a4b99e5a2c4f94b703cfbd0344d23b46
BarakaLab <85062427+Baraka124@users.noreply.github.com>
2026-04-15 10:52:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@
 // FIX 3: Absence FK - recorded_by nullable-safe + full_name in JWT
 // FIX 4: rotation_category Joi/DB enum mismatch corrected
 // FIX 5: research_lines added to rolePermissions
-// FIX 6: Duplicate on-call routes removed
+// FIX 6: Duplicate on-call routes removed       
 // FIX 8: full_name added to JWT payload
 // FIX 9: Absence PUT recalculates total_days + current_status
 // --- NEW FIXES ---
